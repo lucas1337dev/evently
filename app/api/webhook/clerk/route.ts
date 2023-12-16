1bacd18f24bcdf67f1cca1e02fee7a8db1cc79c1
Lucas Gabriel <lucasgbarbosa@yahoo.com>
2023-12-16 13:28:54 -0300

    utils error fixed

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -6,6 +6,7 @@ import { clerkClient } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
+    // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
     const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
     if (!WEBHOOK_SECRET) {
@@ -14,24 +15,29 @@ export async function POST(req: Request) {
         )
     }
 
+    // Get the headers
     const headerPayload = headers()
     const svix_id = headerPayload.get("svix-id")
     const svix_timestamp = headerPayload.get("svix-timestamp")
     const svix_signature = headerPayload.get("svix-signature")
 
+    // If there are no headers, error out
     if (!svix_id || !svix_timestamp || !svix_signature) {
         return new Response("Error occured -- no svix headers", {
             status: 400,
         })
     }
 
+    // Get the body
     const payload = await req.json()
     const body = JSON.stringify(payload)
 
+    // Create a new Svix instance with your secret.
     const wh = new Webhook(WEBHOOK_SECRET)
 
     let evt: WebhookEvent
 
+    // Verify the payload with the headers
     try {
         evt = wh.verify(body, {
             "svix-id": svix_id,
@@ -45,6 +51,7 @@ export async function POST(req: Request) {
         })
     }
 
+    // Get the ID and type
     const { id } = evt.data
     const eventType = evt.type
 
@@ -61,9 +68,9 @@ export async function POST(req: Request) {
         const user = {
             clerkId: id,
             email: email_addresses[0].email_address,
+            username: username!,
             firstName: first_name,
             lastName: last_name,
-            username: username!,
             photo: image_url,
         }
 
@@ -81,14 +88,7 @@ export async function POST(req: Request) {
     }
 
     if (eventType === "user.updated") {
-        const {
-            id,
-            email_addresses,
-            image_url,
-            first_name,
-            last_name,
-            username,
-        } = evt.data
+        const { id, image_url, first_name, last_name, username } = evt.data
 
         const user = {
             firstName: first_name,
