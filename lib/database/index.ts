f88d7a1e2a381d26baba0adae66906930d749bae
Lucas Gabriel <lucasgbarbosa@yahoo.com>
2023-12-18 11:50:33 -0300

    public id fixed

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,12 +2,12 @@ import mongoose from "mongoose"
 
 const MONGODB_URI = process.env.MONGODB_URI
 
-let cached = (global as any).mongoose || { con: null, promise: null }
+let cached = (global as any).mongoose || { conn: null, promise: null }
 
 export const connectToDatabase = async () => {
     if (cached.conn) return cached.conn
 
-    if (!MONGODB_URI) throw new Error("MongoDB URI is missing")
+    if (!MONGODB_URI) throw new Error("MONGODB_URI is missing")
 
     cached.promise =
         cached.promise ||
@@ -16,7 +16,7 @@ export const connectToDatabase = async () => {
             bufferCommands: false,
         })
 
-        cached.conn = await cached.promise
+    cached.conn = await cached.promise
 
-        return cached.conn
+    return cached.conn
 }
