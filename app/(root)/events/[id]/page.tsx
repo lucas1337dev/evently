a07fba816a31beb5af766f61a64cbb4a4db00a94
Lucas Gabriel <lucasgbarbosa@yahoo.com>
2024-01-02 15:44:41 -0300

    pagination added

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,17 +1,23 @@
 import CheckoutButton from "@/components/shared/CheckoutButton"
 import Collection from "@/components/shared/Collection"
-import { getEventById, getRelatedEventsByCategory } from "@/lib/actions/event.actions"
+import {
+    getEventById,
+    getRelatedEventsByCategory,
+} from "@/lib/actions/event.actions"
 import { formatDateTime } from "@/lib/utils"
 import { SearchParamProps } from "@/types"
 import Image from "next/image"
 
-const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) => {
+const EventDetails = async ({
+    params: { id },
+    searchParams,
+}: SearchParamProps) => {
     const event = await getEventById(id)
 
     const relatedEvents = await getRelatedEventsByCategory({
         categoryId: event.category._id,
         eventId: event._id,
-        page: searchParams.page as string
+        page: searchParams.page as string,
     })
 
     return (
@@ -53,10 +59,7 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
                             </div>
                         </div>
 
-                        <CheckoutButton 
-                            event={event}
-                        />
-
+                        <CheckoutButton event={event} />
 
                         <div className="flex flex-col gap-5">
                             <div className="flex gap-2 md:gap-3">
@@ -130,8 +133,8 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
                     emptyStateSubtext="Come back later"
                     collectionType="All_Events"
                     limit={6}
-                    page={1}
-                    totalPages={2}
+                    page={searchParams.page as string}
+                    totalPages={relatedEvents?.totalPages}
                 />
             </section>
         </>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,22 @@
+import CategoryFilter from "@/components/shared/CategoryFilter"
 import Collection from "@/components/shared/Collection"
+import Search from "@/components/shared/Search"
 import { Button } from "@/components/ui/button"
 import { getAllEvents } from "@/lib/actions/event.actions"
+import { SearchParamProps } from "@/types"
 import Image from "next/image"
 import Link from "next/link"
 
-const Home = async () => {
+const Home = async ({ searchParams }: SearchParamProps) => {
+    const page = Number(searchParams?.page) || 1
+    const searchText = (searchParams?.query as string) || ""
+    const category = (searchParams?.category as string) || ""
+
     const events = await getAllEvents({
-        query: "",
-        category: "",
-        page: 1,
-        limit: 6
+        query: searchText,
+        category,
+        page: page,
+        limit: 6,
     })
 
     return (
@@ -52,18 +59,18 @@ const Home = async () => {
                 </h2>
 
                 <div className="flex w-full flex-col gap-5 md:flex-row">
-                    Search 
-                    CategoryFilter
+                    <Search />
+                    <CategoryFilter />
                 </div>
 
-                <Collection 
+                <Collection
                     data={events?.data}
                     emptyTitle="No Events Found"
                     emptyStateSubtext="Come back later"
                     collectionType="All_Events"
                     limit={6}
-                    page={1}
-                    totalPages={2}
+                    page={page}
+                    totalPages={events?.totalPages}
                 />
             </section>
         </>
